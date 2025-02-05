4058c2de8f4ad3d9bbeba77fe9e60133d252cf9c
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-02-05 11:46:48 -0600

    up

--- assets/js/mainFlow-live.js
+++ assets/js/mainFlow-live.js
@@ -434,12 +434,9 @@ submitbtn.addEventListener("click", async (e) => {
 
   // Add an event listener to the "Send Flowers" button
   const sendFlowersButton = document.getElementById("send-flowers");
-  sendFlowersButton.addEventListener("click", incrementFlowerCount);
+ // sendFlowersButton.addEventListener("click", incrementFlowerCount);
   
-  // Add an event listener to the "Send Gift" button
-  const sendGiftButton = document.getElementById("send-gift");
-  sendGiftButton.addEventListener("click", openGiftPopup);
-  
+
 
 const anonymousCheckbox = document.getElementById("anonymousCheckbox");
 const guestNameInput = document.getElementById("guestName");
