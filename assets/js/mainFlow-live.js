01ad01fd945cdf7548b5564b06596d8a903eedb5
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-02-10 14:28:19 -0600

    revvg

--- assets/js/mainFlow-live.js
+++ assets/js/mainFlow-live.js
@@ -451,7 +451,7 @@ if(!entriesDiv){
       const postID = doc.id;
       const timeAgo = timestamp ? timeSincePost(timestamp) : "Unknown time";
 
-      if (entry.status === 'active' && entry.public === 'public'  ) {
+      if (entry.status === 'active' && entry.public !== 'private'   ) {
         // Append guestbook entry
         entriesDiv.innerHTML += `
           <div class="entry" style="border: 1px solid #ddd; padding: 10px; margin-bottom: 10px; border-radius: 8px; background: #f9f9f9;">
@@ -547,7 +547,7 @@ if(submitbtn) submitbtn.addEventListener("click", async (e) => {
         message,
         userIP,
         status: "active",
-        public: privateCheckbox.checked,
+        public: privateCheckbox.checked || true,
         timestamp: serverTimestamp(),
       });
       nameInput.value = ''; // Clear form inputs
