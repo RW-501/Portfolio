c5297d4ea15f73018b81e2a8fe6b71c40898388e
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-02-10 18:51:49 -0600

    zvzv v

--- assets/js/mainFlow-live.js
+++ assets/js/mainFlow-live.js
@@ -555,6 +555,15 @@ window.deleteMessage = deleteMessage;
 
 
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((err) => {
+      console.error('Service Worker registration failed:', err);
+    });
+}
 
 
 
